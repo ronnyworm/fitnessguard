19929e420d1c5afee97340ec681ea0de141d4d92
Ronny Worm <r-worm@gmx.de>
2016-01-24 13:55:45 +0100

    index, compact und show brauchen kein Passwort

--- FitnessGuardRails/app/controllers/runs_controller.rb
+++ FitnessGuardRails/app/controllers/runs_controller.rb
@@ -1,7 +1,7 @@
 class RunsController < ApplicationController
   before_action :set_run, only: [:show, :edit, :update, :destroy]
 
-  http_basic_authenticate_with name: "ronny", password: "bangbangbang"
+  http_basic_authenticate_with name: "ronny", password: "bangbangbang", except: [:index, :show, :compact]
 
   # GET /runs
   # GET /runs.json
