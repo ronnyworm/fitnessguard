7216cfeda4a1d5993416abe16cce377fd031d2cd
Ronny Worm <r-worm@gmx.de>
2016-03-26 01:09:58 +0100

    Added colors by participants for tables

--- FitnessGuardRails/app/helpers/runs_helper.rb
+++ FitnessGuardRails/app/helpers/runs_helper.rb
@@ -2,4 +2,16 @@ module RunsHelper
 	def nl2br(s)
 	  s.gsub(/\n/, '<br>')
 	end
+
+	def bgcolor_by_participants(participants)
+		if participants.include? "Ronny"
+			"#FFCEC3"
+		elsif participants.include? "Markus"
+			"#D8EAFF"
+		elsif participants.include? "Tobias"
+			"#DAFFC8"
+		else
+			"#FFFFFF"
+		end
+	end
 end
